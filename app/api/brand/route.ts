d5a225cca9b1f603141e0ff38eadc84cd8187117
Nsybluu <mosanupat04@gmail.com>
2026-02-24 17:46:42 +0700

    fix api

--- app/api/brand/route.ts
+++ app/api/brand/route.ts
@@ -3,6 +3,6 @@ import { BrandService } from "@/src/lib/services/BrandService";
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  const brands = await BrandService.getAll();
+  const brands = await BrandService.getAllBrands();
   return NextResponse.json(brands);
 }
